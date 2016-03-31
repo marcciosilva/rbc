3f5bb497b9d72c10d8bcf124eb0b943bf81a9b57
Marccio Silva <marccio.silva@fing.edu.uy>
2016-03-31 11:58:57 -0300

    Agregado lo de la ultima clase (modificaciones en camera test y program 5)

--- ParteA/src/com/marcciosilva/rbc/Program5.java
+++ ParteA/src/com/marcciosilva/rbc/Program5.java
@@ -3,11 +3,14 @@ package com.marcciosilva.rbc;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
+import lejos.nxt.comm.RConsole;
 import lejos.util.Delay;
 
 public class Program5 {
 
 	public static void main(String[] args) {
+		RConsole.open();
+		LCD.clear();
 		LCD.drawString("Program5", 0, 0);
 
 		Button.waitForAnyPress();
@@ -15,19 +18,19 @@ public class Program5 {
 		Motor.A.setSpeed(720);
 		Motor.B.setSpeed(720);
 		Motor.C.setSpeed(720);
-
+		String display = "";
 		int i = 0;
 		while (i < 8) {
 			Motor.A.rotate(2 * 360, true);
 			Motor.B.rotate(2 * 360, true);
 			Motor.C.rotate(2 * 360, true);
-			while (Motor.A.isMoving()) {
+			while (Motor.A.isMoving() || Motor.B.isMoving() || Motor.C.isMoving()) {
 				Delay.msDelay(200);
-				LCD.drawString(
-						Integer.toString(Motor.A.getTachoCount()) + " " + Integer.toString(Motor.B.getTachoCount())
-								+ " " + Integer.toString(Motor.A.getTachoCount()) + " ",
-						0, i);
+				display = Integer.toString(Motor.A.getTachoCount()) + " " + Integer.toString(Motor.B.getTachoCount())
+						+ " " + Integer.toString(Motor.A.getTachoCount()) + " ";
+				LCD.drawString(display, 0, i);
 			}
+			RConsole.print(display + "\n");
 			i++;
 		}
 		Button.waitForAnyPress();
