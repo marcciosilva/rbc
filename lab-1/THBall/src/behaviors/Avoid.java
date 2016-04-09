7b059a9ac4faedb39e42abb9202f416094e1bdd9
Marccio Silva <marccio.silva@fing.edu.uy>
2016-04-09 12:52:57 -0300

    Mejorado código de clase pasada Agregado código para pegar la vuelta cuando se llega a la zona muerta Agregada calibración de compás

--- lab-1/THBall/src/behaviors/Avoid.java
+++ lab-1/THBall/src/behaviors/Avoid.java
@@ -1,5 +1,6 @@
 package behaviors;
 
+import lejos.nxt.LCD;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.UltrasonicSensor;
 import lejos.robotics.subsumption.Behavior;
@@ -13,52 +14,52 @@ public class Avoid implements Behavior {
 
 	@Override
 	public boolean takeControl() {
-		return ultrasonicSensor.getDistance() <= 10 || THBall.girando;
+		return ultrasonicSensor.getDistance() <= 10;
 	}
 
 	@Override
 	public void action() {
-		THBall.girando = true;
 		THBall.setSpeed(THBall.SPEED_TURN);
 		leftMotor.resetTachoCount();
 		rightMotor.resetTachoCount();
+		// retrocede hasta estar a por lo menos 20 cm
+		// de algun obstaculo
 		while (ultrasonicSensor.getDistance() < 20) {
 			leftMotor.backward();
 			rightMotor.backward();
-			// Thread.yield();
-			// try {
-			// Thread.sleep(50);
-			// } catch (InterruptedException e) {
-			// LCD.drawString(e.getMessage(), 0, 0);
-			// }
+			Thread.yield();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				LCD.drawString(e.getMessage(), 0, 0);
+			}
 		}
 		leftMotor.stop();
 		rightMotor.stop();
 		try {
-			if (THBall.facingLeft) {
-				turn(-180);
+			if (THBall.goingLeft) {
+				THBall.turn(-180);
 			} else {
-				turn(180);
+				THBall.turn(180);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		while (rightMotor.isMoving() || leftMotor.isMoving()) {
+			Thread.yield();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				LCD.drawString(e.getMessage(), 0, 0);
+			}
+		}
 		THBall.facingLeft = !THBall.facingLeft;
-		THBall.girando = false;
+		THBall.nextRow();
 	}
 
 	@Override
 	public void suppress() {
-		THBall.leftMotor.stop();
-		THBall.rightMotor.stop();
-	}
-
-	public static void turn(int angle) throws Exception {
-		if (angle < 0) {
-			rightMotor.rotate((int) Math.abs(angle * THBall.conversionAngles), false);
-		} else {
-			leftMotor.rotate((int) (angle * THBall.conversionAngles), false);
-		}
+		THBall.stopMotors();
 	}
 
 }
--- lab-1/THBall/src/main/THBall.java
+++ lab-1/THBall/src/main/THBall.java
@@ -1,8 +1,5 @@
 package main;
 
-import behaviors.Avanzar;
-import behaviors.Avoid;
-import behaviors.Corregir;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
@@ -12,60 +9,114 @@ import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.addon.CompassHTSensor;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
+import behaviors.Avanzar;
+import behaviors.Avoid;
+import behaviors.AvoidDeathzone;
+import behaviors.Corregir;
 
 public class THBall {
 
-	public final static int SPEED_DRIVE = 500;
-	public final static int SPEED_TURN = 300;
+	private static Arbitrator arbitrator;
+	// mapa de la pista
+	private static int currentRow = 0;
+	private static int rows = 5;
+	public static boolean goingLeft = false;
+
+	public final static int SPEED_DRIVE = (int) Motor.A.getMaxSpeed();
+	public final static int SPEED_TURN = SPEED_DRIVE / 2;
+	final static int SPEED_CALIBRATION = SPEED_TURN / 4;
 	// facingLeft = true indica que al inicio, el robot
 	// tiene a la zona muerta a su lado izquierdo
 	public static boolean facingLeft = false;
 	public static boolean originalFacing = false;
+	// actuadores
 	public static NXTRegulatedMotor leftMotor = Motor.A;
 	public static NXTRegulatedMotor rightMotor = Motor.C;
+	// sensores
 	public static UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S4);
 	public static CompassHTSensor compass = new CompassHTSensor(SensorPort.S1);
-	public static float anguloOriginal = 0.0f;
+	// public static float anguloOriginal = 0.0f;
 	// conversion de clicks del tacometro a grados
 	public static float conversionAngles = 11.6f;
-	public static boolean girando = false;
 
 	public static void main(String[] args) {
 		// seteo sensor a modo continuo para no tener que
 		// mandar pings manualmente
 		ultrasonicSensor.continuous();
-		Behavior avanzar = new Avanzar();
-		Behavior correct = new Corregir();
-		Behavior avoid = new Avoid();
-
-		Behavior behaviors[] = { avanzar, correct, avoid };
-
-		Arbitrator arbitrator = new Arbitrator(behaviors);
-
+		setupBehaviors();
+		// calibración del compás magnético
+		calibrateCompass();
 		// si se toca escape, sale
-		// si toca right, esta mirando a la derecha
-		// si toca left, esta mirando a la izquierda
-		LCD.drawString("Calibrando...", 0, 0);
-		anguloOriginal = compass.getDegrees();
+		// si toca right, la zona muerta esta a su derecha
+		// si toca left, la zona muerta esta a su izquierda
 		boolean exitMenu = false;
 		LCD.clear();
 		LCD.drawString("Presione algun boton", 0, 0);
+		int buttonPressed = 0;
 		while (!exitMenu) {
-			if ((Button.readButtons() & Button.ID_ESCAPE) != Button.ID_ESCAPE) {
-				if ((Button.readButtons() & Button.ID_LEFT) == Button.ID_LEFT) {
+			buttonPressed = Button.readButtons();
+			if ((buttonPressed & Button.ID_ESCAPE) != Button.ID_ESCAPE) {
+				if ((buttonPressed & Button.ID_LEFT) == Button.ID_LEFT) {
 					LCD.clear();
 					facingLeft = true;
+					goingLeft = true;
 					originalFacing = facingLeft;
 					arbitrator.start();
 					exitMenu = true;
-				} else if ((Button.readButtons() & Button.ID_RIGHT) == Button.ID_RIGHT) {
+				} else if ((buttonPressed & Button.ID_RIGHT) == Button.ID_RIGHT) {
 					facingLeft = false;
+					goingLeft = false;
 					originalFacing = facingLeft;
 					arbitrator.start();
 					exitMenu = true;
 				}
-			}
+			} else exitMenu = true;
 		}
+	}
+
+	private static void setupBehaviors() {
+		Behavior avanzar = new Avanzar();
+		Behavior correct = new Corregir();
+		Behavior avoid = new Avoid();
+		Behavior avoidDeathzone = new AvoidDeathzone();
+		// pongo behaviors en orden de prioridad
+		// a mayor indice mayor prioridad
+		Behavior behaviors[] = { avanzar, correct, avoid, avoidDeathzone };
+		// declaro arbitrator
+		arbitrator = new Arbitrator(behaviors);
+	}
+
+	private static void calibrateCompass() {
+		LCD.clear();
+		LCD.drawString("Calibrando...", 0, 0);
+		// anguloOriginal = compass.getDegrees();
+		compass.startCalibration();
+		setSpeed(SPEED_CALIBRATION);
+		leftMotor.forward();
+		rightMotor.backward();
+		// la idea es que en 40 segundos haga dos vueltas completas
+		// hay que calibrar eso experimentalmente
+		sleep(40000);
+		leftMotor.stop();
+		rightMotor.stop();
+		compass.stopCalibration();
+		// cinco segundos para posicionar al robot donde se quiera
+		sleep(5000);
+		compass.resetCartesianZero();
+	}
+
+	public static void sleep(int tiempo) {
+		try {
+			Thread.sleep(tiempo);
+		} catch (InterruptedException e) {
+			LCD.clear();
+			LCD.drawString(e.getMessage(), 0, 0);
+		}
+	}
+
+	public static void stopMotors() {
+		leftMotor.stop();
+		rightMotor.stop();
 	}
 
 	public static void setSpeed(int speed) {
@@ -75,6 +126,26 @@ public class THBall {
 		} else {
 			leftMotor.setSpeed(SPEED_DRIVE);
 			rightMotor.setSpeed(SPEED_DRIVE);
+		}
+	}
+
+	public static void resetCurrentRow() {
+		currentRow = 0;
+	}
+
+	public static void nextRow() {
+		currentRow = (currentRow++) % rows;
+	}
+
+	public static boolean nextToDeathZone() {
+		return currentRow == rows - 1;
+	}
+
+	public static void turn(int angle) throws Exception {
+		if (angle < 0) {
+			rightMotor.rotate((int) Math.abs(angle * THBall.conversionAngles), true);
+		} else {
+			leftMotor.rotate((int) (angle * THBall.conversionAngles), true);
 		}
 	}
 
